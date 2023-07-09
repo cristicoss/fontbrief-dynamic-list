5bd527958131a1c4238b81efcde0956a47a0a8ed
cristicoss <64747154+cristicoss@users.noreply.github.com>
2023-07-09 10:59:06 +0200

    test 2
    added webflow's custom code to index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 "use strict";
+/*
+<link rel="stylesheet" href="http://127.0.0.1:5500/style-fontbrief.css" />
+<script src="http://127.0.0.1:5500/fonts.js" defer></script>
+<script src="http://127.0.0.1:5500/index.js" defer></script>
+*/
 
 const list = document.querySelector(".filter-dynamic-wrapper");
 const loadingConatainer = document.querySelector(".loading_image");
@@ -59,9 +64,126 @@ async function renderFonts() {
   });
 }
 
+function handleScroll() {
+  // slider for increasing and decreasing the font size
+  var inputRange = document.getElementsByClassName("range")[0],
+    maxValue = 50, // the higher the smoother when dragging
+    speed = 5,
+    currValue,
+    image = document.getElementsByClassName("collection-wrapper")[0],
+    previousValue;
+
+  //========= Inceput Cod pt sliderul de redimensionat===========
+  // set min/max value
+  inputRange.min = -50;
+  inputRange.max = maxValue;
+  previousValue = 0;
+  image.style.width = 50 + "%";
+  maxWidth = "100%";
+  minWidth = "10%";
+
+  // listen for unlock
+  function unlockStartHandler() {
+    // set to desired value
+    currValue = +this.value;
+  }
+
+  function unlockEndHandler() {
+    // store current value
+    currValue = +this.value;
+  }
+
+  // bind events
+  inputRange.addEventListener("mousedown", unlockStartHandler, false);
+  inputRange.addEventListener("mousestart", unlockStartHandler, false);
+  inputRange.addEventListener("mouseup", unlockEndHandler, false);
+  inputRange.addEventListener("touchend", unlockEndHandler, false);
+
+  // move gradient
+  inputRange.addEventListener("input", function () {
+    var currWidth = image.clientWidth;
+    currValue = +this.value;
+    //
+
+    if (this.value > -50) {
+      image.style.width = "27%";
+    }
+    if (this.value > -45) {
+      image.style.width = "30%";
+    }
+    if (this.value > -40) {
+      image.style.width = "33%";
+    }
+    if (this.value > -35) {
+      image.style.width = "35%";
+    }
+    if (this.value > -30) {
+      image.style.width = "40%";
+    }
+    if (this.value > -25) {
+      image.style.width = "45%";
+    }
+    if (this.value > -20) {
+      image.style.width = "50%";
+    }
+    if (this.value > -15) {
+      image.style.width = "55%";
+    }
+    if (this.value > -10) {
+      image.style.width = "60%";
+    }
+    if (this.value > -5) {
+      image.style.width = "65%";
+    }
+    if (this.value > 0) {
+      image.style.width = "70%";
+    }
+    if (this.value > 5) {
+      image.style.width = "75%";
+    }
+    if (this.value > 10) {
+      image.style.width = "78%";
+    }
+    if (this.value > 15) {
+      image.style.width = "85%";
+    }
+    if (this.value > 20) {
+      image.style.width = "90%";
+    }
+    if (this.value > 25) {
+      image.style.width = "93%";
+    }
+    if (this.value > 30) {
+      image.style.width = "95%";
+    }
+    if (this.value > 35) {
+      image.style.width = "97%";
+    }
+    if (this.value > 40) {
+      image.style.width = "100%";
+    }
+  });
+
+  function myFunction(x) {
+    if (x.matches) {
+      //
+      image.style.width = "100%";
+
+      //
+    } else {
+      image.style.width = "50%";
+    }
+  }
+
+  var x = window.matchMedia("(max-width: 700px)");
+  myFunction(x); // Call listener function at run time
+  x.addListener(myFunction);
+}
+
 renderFonts().then(
   loadingConatainer.classList.add("hidden"),
-  list.classList.remove("hidden")
+  list.classList.remove("hidden"),
+  handleScroll()
 );
 
 console.log("live server");
